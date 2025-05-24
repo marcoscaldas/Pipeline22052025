b4b137f8da6ff1d1469f1c827283df6926b2eaf4
unknown <professor@professor.com.br>
2025-05-24 10:20:47 -0300

    forcando o erro novamene

--- test/example.test.js
+++ test/example.test.js
@@ -3,6 +3,6 @@ const assert = require('assert');
 
 describe('Exemplo de Teste', function() {
   it('deve somar dois números corretamente', function() {
-    assert.strictEqual(1 + 12, 13);
+    assert.strictEqual(1 + 12, 1);
   });
 });
